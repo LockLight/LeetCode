8692f1f756341585aecf4540dd0124741079287a
locklight <locklightz@gmail.com>
2018-08-12 17:43:38 +0800

    remove linked list elements

---
+++ LeetCode.playground/Pages/Remove Linked List Elements.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+//删除链表中等于给定值 val 的所有节点。
+//
+//示例:
+//
+//输入: 1->2->6->3->4->5->6, val = 6
+//输出: 1->2->3->4->5
+
+import Foundation
+
+public class ListNode{
+    public var val:Int
+    public var next:ListNode?
+    public init(_ val:Int){
+        self.val = val
+        self.next = nil
+    }
+}
+
+func removeElements(_ head:ListNode?,_ val:Int) -> ListNode?{
+    let dummy  = ListNode(0)
+    dummy.next =  head
+    var node = dummy
+    
+    while node.next != nil {
+        if node.next!.val == val{
+            node.next = node.next!.next
+        }else{
+            node = node.next!
+        }
+    }
+    return dummy.next
+}
